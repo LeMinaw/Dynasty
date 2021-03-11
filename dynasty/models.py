45875c069209c45fe8b0a72c398127b1eb21eca2
LeMinaw <leminaw@gmail.com>
2021-03-11 12:02:57 +0200

    add qt model for rel_model enumeration

---
+++ dynasty/models.py
@@ -0,0 +1,32 @@
+"""This module contains Qt models to interface Qt views with Qt-agnostic data
+types.\n
+For consistency with Qt, this file uses camelCase for variables, instances and
+functions names.
+"""
+
+from enum import Enum
+from PyQt5.QtCore import Qt, QAbstractListModel
+
+from dynasty.walkers import RelModel
+
+
+class EnumModel(QAbstractListModel):
+    """Qt list model around a Python enumeration."""
+    def __init__(self, enum: Enum, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.enum = enum
+
+    def data(self, index, role):
+        entry = self.enum(index.row())
+
+        if role == Qt.DisplayRole:
+            return str(entry)
+        return None
+
+    def rowCount(self, index):
+        # pylint: disable = unused-argument
+        return len(self.enum)
+
+
+REL_MODELS_MODEL = EnumModel(RelModel)
